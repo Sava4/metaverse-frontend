deab4df172859932076836c792ea862c8e5b48f5
brain-metaverse <geovannyrafael0810@gmail.com>
2025-10-07 02:29:33 +0900

    change the database

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -8,14 +8,14 @@ import { getDatabase, ref, set, push, get } from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-x66fvhFKbvYVgZekleH9u1Tai8stE4E",
-  authDomain: "brainbot-8d0b5.firebaseapp.com",
-  databaseURL: "https://brainbot-8d0b5-default-rtdb.firebaseio.com",
-  projectId: "brainbot-8d0b5",
-  storageBucket: "brainbot-8d0b5.firebasestorage.app",
-  messagingSenderId: "1017628838910",
-  appId: "1:1017628838910:web:ee3eb53e7c3c6168f08eaf",
-  measurementId: "G-K9GXN96QL9"
+  apiKey: "AIzaSyBxx7CtxuSBW6hcCK6R9OSFOvItMRykvzI",
+  authDomain: "brainbotai-9b32e.firebaseapp.com",
+  databaseURL: "https://brainbotai-9b32e-default-rtdb.firebaseio.com",
+  projectId: "brainbotai-9b32e",
+  storageBucket: "brainbotai-9b32e.firebasestorage.app",
+  messagingSenderId: "880636780931",
+  appId: "1:880636780931:web:a6623a4308dcd172f996b6",
+  measurementId: "G-62SWH2YGCE"
 };
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCvi7MhE_qgLgkgPr3JUT8BZXMYfGTwwso",
